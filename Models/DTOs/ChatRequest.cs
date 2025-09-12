7fbae986fc5cd0161f125de340998dbfbeb7c4fd
Collin Herber <rustaddicts@gmail.com>
2025-09-12 16:03:45 -0400

    feat: add in chat color and chat icon options

--- Models/DTOs/ChatRequest.cs
+++ Models/DTOs/ChatRequest.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+
 namespace PoTSharedModules.Models.DTOs;
 
 public class ChatRequest
@@ -5,4 +7,40 @@ public class ChatRequest
 	public required string PlayerName { get; set; }
 	public required string Message { get; set; }
 	public required string SteamId { get; set; }
+}
+
+public enum ChatColorOption
+{
+	[Description("#1abc9c")]
+	Supporter,
+    
+	[Description("#9b59b6")]
+	ProSupporter,
+    
+	[Description("#e74c3c")]
+	EliteSupporter,
+    
+	[Description("#9fc1ff")]
+	UltimateSupporter,
+    
+	[Description("#1e86fe")]
+	Developer,
+    
+	[Description("#aa3b3b")]
+	ScottieKnowz
+}
+
+public enum ChatIconOption
+{
+	[Description("Developer")]
+	Developer,
+    
+	[Description("WorldrendSupporter")]
+	WorldrendSupporter,
+    
+	[Description("RiftbornSupporter")]
+	RiftbornSupporter,
+    
+	[Description("AtherionSupporter")]
+	AtherionSupporter,
 }
